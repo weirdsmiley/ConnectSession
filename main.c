50f517bc79228b5b3ce760894b2cf140d2f54c43
Manas <manas18244@iiitd.ac.in>
2019-10-10 03:24:40 +0530

    main file added

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int check_connection()
+{
+	/* Returns 0 if connection is established,
+	 * otherwise -1
+	 */
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	int errno;
+	char network_off[] = "nmcli networking off";
+	char network_on[]  = "nmcli networking on";
+
+	while(1)
+	{
+		if(!check_connection())
+		{
+			system(network_off);
+			system(network_on);
+		}
+		sleep(3);
+	}
+	return 0;
+}
